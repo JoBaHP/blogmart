d2d53bb38c02e3d819538ef57e5a36e86740175d
JoBaHP <jocoraos@yahoo.com>
2022-02-26 20:03:21 +0100

    CHANGES

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,33 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-import Link from 'next/link';
-import { getCategories } from '../services';
+import Link from 'next/link'
+import { getCategories } from '../services'
 
 const Header = () => {
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([])
 
   useEffect(() => {
     getCategories().then((newCategories) => {
-      setCategories(newCategories);
-    });
-  }, []);
+      setCategories(newCategories)
+    })
+  }, [])
 
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
-        <div className="md:float-left block">
+    <div className="container mx-auto mb-8 px-10">
+      <div className="inline-block w-full border-b border-blue-400 py-8">
+        <div className="block md:float-left">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">Advocado</span>
+            <span className="cursor-pointer text-4xl font-bold text-white">
+              TIP-TOP
+            </span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
           {categories.map((category, index) => (
-            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+            <Link key={index} href={`/category/${category.slug}`}>
+              <span className="mt-2 ml-4 cursor-pointer align-middle font-semibold text-white md:float-right">
+                {category.name}
+              </span>
+            </Link>
           ))}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
